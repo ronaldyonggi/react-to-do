3a85e626b7e81eaef102ad1c2743a268b417fc22
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-07 13:39:30 -0400

    added input field

--- src/components/components/components/InputTodo.jsx
+++ src/components/components/components/InputTodo.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const InputTodo = () => {
-  return <div>Input field here...</div>
+  const [title, setTitle] = useState('');
+
+  const handleChange = e => setTitle(e.target.value);
+
+  return (
+    <form>
+      <input 
+      type="text" 
+      placeholder="Add a todo..."
+      value={title}
+      onChange={handleChange}
+      />
+      <input type="submit" />
+    </form>
+  )
 }
 
 export default InputTodo;

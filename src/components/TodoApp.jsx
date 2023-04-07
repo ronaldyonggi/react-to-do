9f60aaf6c8a8e4679a38ce807ba5121cf3a2916e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-07 11:55:11 -0400

    initial items commit

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/components/Header";
+import TodosLogic from "@/components/components/TodosLogic";
+
+const TodoApp = () => {
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+  );
+};
+
+export default TodoApp;

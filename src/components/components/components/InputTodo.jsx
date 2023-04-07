fca3cf838fb89c464116cd53bbc8d7a35ececbe8
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-07 17:35:17 -0400

    new to-do functionality implementation

--- src/components/components/components/InputTodo.jsx
+++ src/components/components/components/InputTodo.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 
-const InputTodo = () => {
+const InputTodo = ({setTodos}) => {
   const [title, setTitle] = useState('');
 
   const handleChange = e => setTitle(e.target.value);
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTodo(title);
+    setTitle('');
+  }
+
+  const addTodo = title => {
+    const newTodo = {
+      id: Math.floor(Math.random() * 10000),
+      title: title,
+      completed: false
+    }
+
+    setTodos(prevState => prevState.concat(newTodo))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input 
       type="text" 
       placeholder="Add a todo..."
